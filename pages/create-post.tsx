aa4542149bf13e124dd1ed35f8d1e7b590b14406
kongo-28 <ryota.tennis.ku@gmail.com>
2024-09-14 19:15:18 +0900

    新規投稿フォームを作成

--- pages/create-post.tsx
+++ pages/create-post.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import styles from "@/styles/Home.module.css"
 
 const CreatePost = () => {
   return (
-    <div>create-post</div>
+
+    <div className={styles.container}>
+      <h1>bブログ新規登録</h1>
+      <form>
+        <label>タイトル</label>
+        <input type="text" />
+        <label>本文</label>
+        <textarea></textarea>
+        <button type="submit"></button>
+
+      </form>
+    </div>
   )
 }
 
