93ccc8b1d0af0b089fbdcba97555eaf9f709b184
kongo-28 <ryota.tennis.ku@gmail.com>
2024-09-14 19:18:17 +0900

    新規投稿ページのレイアウト

--- pages/create-post.tsx
+++ pages/create-post.tsx
@@ -5,13 +5,15 @@ const CreatePost = () => {
   return (
 
     <div className={styles.container}>
-      <h1>bブログ新規登録</h1>
-      <form>
-        <label>タイトル</label>
-        <input type="text" />
-        <label>本文</label>
-        <textarea></textarea>
-        <button type="submit"></button>
+      <h1 className={styles.title}>ブログ新規登録</h1>
+      <form className={styles.form}>
+        <label className={styles.label}>タイトル</label>
+        <input type="text" className={styles.input} />
+        <label className={styles.label}>本文</label>
+        <textarea className={styles.textarea}></textarea>
+        <button type="submit" className={styles.button}>
+          投稿
+        </button>
 
       </form>
     </div>
