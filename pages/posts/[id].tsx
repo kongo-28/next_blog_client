01ae7c81c79a6bce2cecbadce16d3271000f7dd4
kongo-28 <ryota.tennis.ku@gmail.com>
2024-09-14 16:15:34 +0900

    詳細ページのgetStaticpathを指定

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,6 +1,43 @@
+import type {Post} from "@/types"  //isolatedModulesのエラー回避のため、型としてのみインポート
 import React from "react";
 
-const Post = () => {
+
+type Props ={
+    post: Post;
+}
+// pages/posts[:id].tsx
+export async function getStaticPaths() {
+  const res = await fetch("http://localhost:3000/posts");
+  const posts: Post[] = await res.json();
+
+  const paths = posts.map((post) => ({
+    params: { id: post.id.toString() },
+  }));
+
+  return {
+    // paths: [
+    //   { params: { id: 'posts/1' } },
+    // ],
+    paths,
+    fallback: true,
+  }
+}
+
+export async function getStaticProps({params}: {params: {id: string}} ) {
+  const res = await fetch(`http://localhost:3000/posts/${params.id}`);
+  const post = await res.json();
+
+  console.log(post);
+
+  return {
+    props: {
+      post,
+    },
+    revalidate: 60,
+  };
+}
+
+const Post = ( {post}: Props ) => {
   return <div>詳細ページです</div>;
 ;}
 
